afea86ef79482324e313d62a78acc9d1b2f1fcea
Deepanshu Bhatt <48888110+deeshubhatt07@users.noreply.github.com>
2022-12-08 23:10:45 +0530

    Added dummy tweets

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,26 +1,25 @@
 import React, { useState } from 'react'
 import {
     useNavigate
-  } from 'react-router-dom'
-  
+} from 'react-router-dom'
+
 
 const Login = () => {
-    const [username,setUsername] = useState('');
-    const [password,setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
     const navigate = useNavigate();
 
     const login = () => {
-        console.log("Clicked "+username+" , "+password);
-        if(username==='abc' && password==='123')
-        {
+        console.log("Clicked " + username + " , " + password);
+        if (username === 'abc' && password === '123') {
             navigate('/home');
         }
     }
 
     return (
         <div>
-            <input type="text" placeholder="Enter your username" value={username} onChange={(e)=>setUsername(e.target.value)} />
-            <input type="password" placeholder="Enter your password" value={password} onChange={(e)=>setPassword(e.target.value)} />
+            <input type="text" placeholder="Enter your username" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <input type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)} />
             <div>
                 <button onClick={login}>Login</button>
                 <span>or</span>
--- src/Components/Tweet.jsx
+++ src/Components/Tweet.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Tweets from '../data/tweets.json'
 
 const Tweet = () => {
   return (
-    <h1>
-      Tweet
-    </h1>
+    <div>
+      <div><h1>Photo</h1></div>
+      {Tweets.map((tweet, index) => {
+        return (
+          <div id={tweet.id}>
+            <div>
+              <h5>{tweet.name} (Position:{tweet.depart})</h5>
+              <p>Age: {tweet.age} Salary:{tweet.sal}</p>
+            </div>
+          </div>)
+      })}
+    </div>
   )
 }
 
